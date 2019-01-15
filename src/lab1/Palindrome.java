9b4725a9e2a5493fe37252b3b68761f943209b6c
w01266833 <w01266833@Athena.cs.weber.edu>
2019-01-15 08:27:13 -0700

    For each

--- src/lab1/Palindrome.java
+++ src/lab1/Palindrome.java
@@ -15,10 +15,15 @@ public class Palindrome {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        for(string s in new String()){
+        
+        }
        String o1 = "Hello";
        String o2 = "Hel";
        o2 += "lo";
        System.out.println(o1.equals(o2));
+       System.out.println(args.length);
+       System.out.println(args[0]);
        if (args.length == 0)		// version 1
         {
             System.out.println("USAGE: java Palindrome <name>");
